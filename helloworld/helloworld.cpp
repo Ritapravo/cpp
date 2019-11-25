69c07ca8ebac9469963a515c0a69d1615b2b5559
Ritapravo <ritopravosarker@gmail.com>
2019-11-25 22:21:17 +0530

    changes done

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include<stdio.h>
-#include <conio.h>
+#include<stdlib.h>
+#include<time.h>
 
-using namespace std;
+//using namespace std;
 
 void insertionsort(int array[], int n){
     for (int j = 1; j < n; j++)
@@ -20,21 +18,56 @@ void insertionsort(int array[], int n){
 }
 
 int main()
-{
-
-    int n,i;
-    printf("Enter the number of elements: ");
-    scanf("%d",&n);
-    int array[6];
-    for ( i = 0; i < n; i++)
+{   int p=10;
+    int sizes[p] = {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
+    int times[10];
+    for (int k = 0; k < p; k++)
     {
-        scanf("%d",&array[i]);
+        int n = sizes[k];
+
+        int array[n];
+        for(int i = 0; i<n; i++) 
+            array[i] = rand();
+        
+        clock_t t;
+        t = clock();
+
+        insertionsort(array,n);
+
+        t = clock() - t; 
+        double time_taken = ((double)t)/CLOCKS_PER_SEC;
+
+        int d = (int)(time_taken*1000000);//converting to microseconds
+           
+        times[k]= d;
     }
-    insertionsort(array,n);
+    printf("\n");
+    for (int k = 0; k < p; k++){
+        sizes[k]=sizes[k]/100;
+        printf("%d ",sizes[k]);
+    }
+    printf("\n\n");
+    for (int k = 0; k < p; k++){
+        times[k]=times[k]/2000;
+        printf("%d ",times[k]);
+    }
+    printf("\n\n");
     
-    for ( i = 0; i<n; i++)
-    {
-        printf("%d ",array[i]);
+    int graph[51][101]={0};
+    for (int k = 0; k < p; k++){
+        graph[times[k]][sizes[k]] = 1;
     }
-
+    for(int i=50;i>=0;i--){
+        for(int j =0;j<101;j++){
+            if (graph[i][j]==1)
+                printf("*");
+            else
+            {
+                printf(" ");
+            }
+            
+        }
+        printf("\n");
+    }    
+    return 0;
 }
