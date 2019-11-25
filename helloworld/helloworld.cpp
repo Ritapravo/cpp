75667f9a51a6f581f08f1915b3fdac20f9c43707
Ritapravo <ritopravosarker@gmail.com>
2019-11-25 21:22:08 +0530

    graph

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -6,18 +6,35 @@
 
 using namespace std;
 
+void insertionsort(int array[], int n){
+    for (int j = 1; j < n; j++)
+    {
+        int key = array[j];
+        int i=j-1;
+        while(i>=0 && array[i]>key){
+            array[i+1]=array[i];
+            i-=1;
+        }
+        array[i+1]=key;
+    }
+}
+
 int main()
 {
 
-    //cout<<"hello";
-    cout << endl; 
-    int a;
-    printf("Enter a number: ");
-    scanf("%d",&a);
-    printf("You entered: %d",a);
-
-    //getch();
-
-    return 0;
+    int n,i;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int array[6];
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    insertionsort(array,n);
+    
+    for ( i = 0; i<n; i++)
+    {
+        printf("%d ",array[i]);
+    }
 
 }
--- helloworld/ritoman.cpp
+++ helloworld/ritoman.cpp
@@ -1,29 +1,89 @@
 #include<stdio.h>
-#include <conio.h>
+#include<stdlib.h>
+#include <chrono> 
+#include <iostream> 
+using namespace std; 
+using namespace std::chrono;
 
 //using namespace std;
 
-int main()
-{
-
-    //cout<<"hello";
-    //cout << endl; 
-    int n = 5;
-    for (int i = 1; i <= n; i++)
+void insertionsort(int array[], int n){
+    for (int j = 1; j < n; j++)
     {
-        for (int j = 1; j <= i; j++)
-        {
-            printf("%d ",j);
+        int key = array[j];
+        int i=j-1;
+        while(i>=0 && array[i]>key){
+            array[i+1]=array[i];
+            i-=1;
+        }
+        array[i+1]=key;
+    }
+}
+
+int main()
+{   int p=10;
+    int sizes[p] = {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
+    int times[10];
+    for (int k = 0; k < p; k++)
+    {
+        int n = sizes[k];
+
+
+        auto start = high_resolution_clock::now(); 
+        int array[n];
+        for(int i = 0; i<n; i++) 
+            array[i] = rand();
+
+        insertionsort(array,n);
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start); 
+
+        int d = duration.count();
+        /* cout << "\nTime taken by function: "
+            << duration.count() << " microseconds "<<n << endl;   */      
+        times[k]= d;
+    }
+    printf("\n");
+    for (int k = 0; k < p; k++){
+        sizes[k]=sizes[k]/100;
+        printf("%d ",sizes[k]);
+    }
+    printf("\n\n");
+    for (int k = 0; k < p; k++){
+        times[k]=times[k]/2000;
+        printf("%d ",times[k]);
+    }
+    printf("\n\n");
+    
+    int graph[51][101]={0};
+    for (int k = 0; k < p; k++){
+        graph[times[k]][sizes[k]] = 1;
+    }
+    for(int i=50;i>=0;i--){
+        for(int j =0;j<101;j++){
+            if (graph[i][j]==1)
+                printf("*");
+            else
+            {
+                printf(" ");
+            }
             
         }
         printf("\n");
-        
-    }
-    
-    
-
-    //getch();
-
+    }    
     return 0;
-
 }
+
+
+
+
+/* auto start = high_resolution_clock::now(); 
+    int a[500000];
+    for(int i = 0; i<500000; i++) 
+        a[i] = rand(); 
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start); 
+  
+    cout << "\nTime taken by function: "
+         << duration.count() << " microseconds" << endl; */
