1b809dd10dc334c30c4657530ae7d41e21d20395
Ritapravo <ritopravosarker@gmail.com>
2020-04-20 20:26:45 +0530

    20-04-20

---
+++ random/26.(no of subsets with a definite sum).cpp
@@ -0,0 +1,54 @@
+//number of subsets having a definite sum
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+#define int long long int
+#define v(x) vector<x>
+using namespace std;
+void print(int row, int col);
+int subsetno(int arr[], int n , int s);
+int dp[101][1001];
+
+
+
+signed main(){
+    cout<<"Hello Ritoman"<<endl;
+    memset(dp, -1, sizeof(dp));
+    int arr[] = {2,3,5,6,8,10};
+    int n  = sizeof(arr)/sizeof(arr[0]);
+    int s = 10;
+    cout<<subsetno(arr, n, s)<<endl;
+    //print(n+1, s+1);
+    return 0;
+}
+
+int subsetno(int arr[], int n , int s){
+    if(s==0){
+        dp[n][s] = 1;
+        return dp[n][s];
+    }
+    if(n==0){
+        dp[n][s] = 0;
+        return dp[n][s];
+    }
+    if(dp[n][s]!=-1){
+        return dp[n][s];
+    }
+    if(arr[n-1]>s){
+        dp[n][s] = subsetno(arr, n-1, s);
+        return dp[n][s];
+    }
+    else{
+        dp[n][s] = subsetno(arr,n-1,s) + subsetno(arr, n-1, s-arr[n-1]);
+        return dp[n][s];
+    }
+}
+
+void print(int row, int col){
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
