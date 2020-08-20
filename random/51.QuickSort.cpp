23651a25ccb4b591cb9dd68c8704b88d8913f16a
Ritapravo <ritopravosarker@gmail.com>
2020-08-21 02:44:47 +0530

    21-08-2020

---
+++ random/51.QuickSort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+signed main(){
+    cout<<"Hello Ritoman"<<endl;
+    int arr[] = {35,20,40,50,30,60,15,75};
+    int n = sizeof(arr)/sizeof(int);
+    cout<<n<<endl;
+    return 0;
+}
